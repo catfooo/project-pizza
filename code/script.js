d39e46c1583f9ccfb31b5e6a80480b68cd682398
So Youn Choi <manghatuhyo@gmail.com>
2023-09-04 15:07:16 +0100

    1

--- code/script.js
+++ code/script.js
@@ -1,5 +1,7 @@
 console.log("hello");
 const img = document.createElement("img");
 img.src = "./assets/bot.png";
+img.style.width = "777px"
+img.style.height = "777px"
 const bildContainer = document.getElementById("bild-container");
 bildContainer.appendChild(img);
