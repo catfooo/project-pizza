f799283869e7e18495c38390398c487965c76166
So Youn Choi <manghatuhyo@gmail.com>
2023-09-04 15:24:57 +0100

    bilden har klickats

--- code/script.js
+++ code/script.js
@@ -5,3 +5,9 @@ img.style.width = "777px"
 img.style.height = "777px"
 const bildContainer = document.getElementById("bild-container");
 bildContainer.appendChild(img);
+img.addEventListener('mouseover', function() {
+  img.style.cursor = 'pointer';
+});
+img.addEventListener("click", function() {
+  alert("bilden har klickats!");
+});
